493b834e9e1a1396a824e1dabf7ecce55f955435
kevin <kenso.metal@gmail.com>
2016-03-04 08:32:05 -0500

    Items Model

---
+++ db/migrate/20160304133020_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :content
+      t.references :todo_list, index: true
+
+      t.timestamps
+    end
+  end
+end
